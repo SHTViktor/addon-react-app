01a5ac3e8c374e547acb515ab1a158cb5fd08745
Viktor <viktor.s@saasjet.com>
2024-10-31 20:55:26 +0200

    Try comment for heroku

--- server/app.js
+++ server/app.js
@@ -89,7 +89,7 @@ async function getdb() {
 // Mount the static resource dir
     app.use(express.static(staticDir));
     app.use(express.static(path.join(__dirname, 'node_modules')));
-    app.use('/dist', express.static(staticDir));
+    // app.use('/dist', express.static(staticDir));
 // Show nicer errors when in dev mode
     if (devEnv) app.use(errorHandler());
 
