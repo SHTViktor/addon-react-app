e8e9b735175bb483c657a9ada8e78d2c317b0e6d
Viktor <viktor.s@saasjet.com>
2024-11-04 20:09:10 +0200

    Added boardConfig route, and minor code changes

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,21 +1,25 @@
 require('dotenv').config()
 
 module.exports = function (app, addon) {
+
+    const jiraDomain = process.env.REACT_APP_JIRA_DOMAIN;
+    const apiToken = process.env.REACT_APP_JIRA_API_TOKEN;
+    const email = process.env.REACT_APP_EMAIL
+
+    const credentials = `${email}:${apiToken}`;
+    const base64Credentials = Buffer.from(credentials).toString('base64');
+
+    const headers = {
+        'Authorization': `Basic ${base64Credentials}`,
+        'Accept': 'application/json'
+    }
+
     app.get('/api/filters', async function (req, res) {
-
-        const jiraDomain = process.env.REACT_APP_JIRA_DOMAIN;
-        const apiToken = process.env.REACT_APP_JIRA_API_TOKEN;
-
-        const credentials = `vshtinda23@gmail.com:${apiToken}`;
-        const base64Credentials = Buffer.from(credentials).toString('base64');
 
         try {
             const response = await fetch(`${jiraDomain}/rest/api/3/filter/favourite`, {
                 method: 'GET',
-                headers: {
-                    'Authorization': `Basic ${base64Credentials}`,
-                    'Accept': 'application/json'
-                }
+                headers: headers
             });
 
 
@@ -31,33 +35,43 @@ module.exports = function (app, addon) {
         }
     });
 
-    app.get('/api/status', async function (req, res) {
-
-        const jiraDomain = process.env.REACT_APP_JIRA_DOMAIN;
-        const apiToken = process.env.REACT_APP_JIRA_API_TOKEN;
-
-        const credentials = `vshtinda23@gmail.com:${apiToken}`;
-        const base64Credentials = Buffer.from(credentials).toString('base64');
+    app.get('/api/boardConfig', async function (req, res) {
 
         try {
-            const response = await fetch(`${jiraDomain}/rest/api/3/status`, {
+            const response = await fetch(`${jiraDomain}/rest/agile/1.0/board/1/configuration`, {
                 method: 'GET',
-                headers: {
-                    'Authorization': `Basic ${base64Credentials}`,
-                    'Accept': 'application/json'
-                }
+                headers: headers
             });
 
-
             if (!response.ok) {
-                throw new Error('Failed to fetch filters');
+                throw new Error('Failed to fetch statuses');
             }
 
             const data = await response.json();
             res.json(data);
         } catch (error) {
-            console.error('Error fetching filters:', error);
-            res.status(500).send('Error fetching filters');
+            console.error('Error fetching statuses:', error);
+            res.status(500).send('Error fetching statuses');
+        }
+    });
+
+    app.get('/api/search', async function (req, res) {
+
+        try {
+            const response = await fetch(`${jiraDomain}/rest/api/3/search`, {
+                method: 'GET',
+                headers: headers
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch search');
+            }
+
+            const data = await response.json();
+            res.json(data);
+        } catch (error) {
+            console.error('Error fetching search:', error);
+            res.status(500).send('Error fetching search');
         }
     });
 
