9e9341941e76e771e89af4d550c9d95e5656056c
Viktor <viktor.s@saasjet.com>
2024-11-04 14:20:34 +0200

    Created function to selector component to fetch filters

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import '../styles/index.css';
 
-export default function Selector() {
+export default function Selector(props) {
 
+    const [filters, setFilters] = useState([]);
 
+    useEffect(() => {
+        async function fetchJiraFilters() {
+            try {
+                const response = await fetch(`/api/filters`, {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch filters');
+                }
+
+                const data = await response.json();
+                setFilters(data);
+
+            } catch (error) {
+                console.error('Error fetching filters:', error);
+            }
+        }
+
+        fetchJiraFilters();
+    }, []);
+
+    const optionsElements = filters.map(filter => (
+        <option key={filter.id} value={filter.name}>{filter.name}</option>
+    ));
 
     return (
         <div className="filter-container">
             <label htmlFor="issue-filter">Select issues by filter:</label>
             <select id="issue-filter">
-                <option value="">default filter</option>
+                <option value="">Select filter</option>
+                {optionsElements}
             </select>
         </div>
     )
