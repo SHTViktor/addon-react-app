bc5b88aa6886d098c647541b5a9976ea72fbba04
Viktor <viktor.s@saasjet.com>
2024-11-06 13:39:01 +0200

    Remove states in selector component and use parents state, and added useEffect for fetching filters jql

--- src/components/Selector.jsx
+++ src/components/Selector.jsx
@@ -1,15 +1,21 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 
 import '../styles/index.css';
 
 export default function Selector(props) {
 
-    const [filters, setFilters] = useState([]);
+    const filters = props.filters;
+    const setFilters = props.setFilters;
+
+    const selectedFilter = props.selectedFilter;
+    const setSelectedFilter = props.setSelectedFilter;
+
+    const setSelectedFiltersJQL = props.setSelectedFiltersJQL;
 
     useEffect(() => {
         async function fetchJiraFilters() {
             try {
-                const response = await fetch(`/api/filters`, {
+                const response = await fetch(`/api/filters/`, {
                     method: 'GET',
                     headers: {
                         'Accept': 'application/json'
@@ -22,7 +28,6 @@ export default function Selector(props) {
 
                 const data = await response.json();
                 setFilters(data);
-
             } catch (error) {
                 console.error('Error fetching filters:', error);
             }
@@ -31,14 +36,47 @@ export default function Selector(props) {
         fetchJiraFilters();
     }, []);
 
+    useEffect(() => {
+        if (selectedFilter) {
+            async function fetchJiraFilterJQL() {
+                try {
+                    const response = await fetch(`/api/filters/${selectedFilter}`, {
+                        method: 'GET',
+                        headers: {
+                            'Accept': 'application/json'
+                        }
+                    });
+
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch filter');
+                    }
+
+                    const data = await response.json();
+                    setSelectedFiltersJQL(data.jql);
+                } catch (error) {
+                    console.error('Error fetching filter:', error);
+                }
+            }
+
+            fetchJiraFilterJQL();
+        } else {
+            setSelectedFiltersJQL("");
+        }
+    }, [selectedFilter, setSelectedFiltersJQL]);
+
     const optionsElements = filters.map(filter => (
-        <option key={filter.id} value={filter.name}>{filter.name}</option>
+        <option key={filter.id} value={filter.id}>{filter.name}</option>
     ));
+
+    function changeSelector(event){
+        const value = event.target.value;
+        setSelectedFilter(value);
+    }
 
     return (
         <div className="filter-container">
             <label htmlFor="issue-filter">Select issues by filter:</label>
-            <select id="issue-filter">
+            <select id="issue-filter" onChange={changeSelector}>
                 <option value="">Select filter</option>
                 {optionsElements}
             </select>
