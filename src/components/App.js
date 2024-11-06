5944826e31c28618cbca1faab8164f0b58d30f34
Viktor <viktor.s@saasjet.com>
2024-11-06 13:37:31 +0200

    Created states in App component for access in child components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import '../styles/index.css';
 import Selector from "./Selector";
@@ -6,10 +6,33 @@ import Table from "./Table";
 
 function App() {
 
+    const [issues, setIssues] = useState([]);
+    const [statuses, setStatuses] = useState([]);
+
+    const [filters, setFilters] = useState([]);
+    const [selectedFilter, setSelectedFilter] = useState();
+    const [selectedFilterJQL, setSelectedFilterJQL] = useState();
+
     return (
         <div className="App">
-            <Selector />
-            <Table />
+            <Selector
+                filters={filters}
+                setFilters={setFilters}
+                selectedFilter={selectedFilter}
+                setSelectedFilter={setSelectedFilter}
+                selectedFilterJQL={selectedFilterJQL}
+                setSelectedFiltersJQL={setSelectedFilterJQL}
+            />
+
+            <Table
+                issues={issues}
+                setIssues={setIssues}
+                statuses={statuses}
+                setStatuses={setStatuses}
+                selectedFilter={selectedFilter}
+                setSelectedFilter={setSelectedFilter}
+                selectedFilterJQL={selectedFilterJQL}
+            />
         </div>
     );
 }
